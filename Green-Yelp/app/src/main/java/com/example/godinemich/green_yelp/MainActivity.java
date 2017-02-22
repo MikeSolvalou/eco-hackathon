ec673bc6125615e59517a09103e3eb9a0d8ad663
Deborah Crook <deborahcrook01@gmail.com>
2017-02-23 11:19:23 +1300

    Added onSearch method that gets user location

--- Green-Yelp/app/src/main/java/com/example/godinemich/green_yelp/MainActivity.java
+++ Green-Yelp/app/src/main/java/com/example/godinemich/green_yelp/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.godinemich.green_yelp;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.location.*;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    Location userLocation;
+
+    LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+            userLocation = location;
+            Log.i("Latitude",userLocation.getLatitude() + "");
+            Log.i("Longitude",userLocation.getLongitude() + "");
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test comment
+        onSearch();
+    }
+
+    protected void onSearch(){
+        try{
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            //run search algorithm to display restaurants
+        }
+        catch(SecurityException ex){
+            //do something
+            Log.i("Test","Permission failed");
+        }
     }
 }
