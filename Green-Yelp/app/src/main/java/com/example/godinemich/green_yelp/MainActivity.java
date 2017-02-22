24b0de16a063a920e38c03bb549bde1694a0525a
MikeSolvalou <godinemich@myvuw.ac.nz>
2017-02-23 11:19:29 +1300

    Added basic main menu screen from tutorial.

--- Green-Yelp/app/src/main/java/com/example/godinemich/green_yelp/MainActivity.java
+++ Green-Yelp/app/src/main/java/com/example/godinemich/green_yelp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.godinemich.green_yelp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         //test comment
+        mainMenu();
     }
+
+
+    public void mainMenu(){
+        setContentView(R.layout.activity_main);     //splash screen
+
+        //connect splash screen buttons
+        Button btn1=(Button)findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                TextView txt=(TextView)findViewById(R.id.txt);
+                txt.setText("Pressed");
+            }
+        });
+
+    }
+
 }
